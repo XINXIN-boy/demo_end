5cea228ae111d6c61e6e108d1f3938b2d5424724
xinxin <1400956093@qq.com>
2020-11-05 09:15:13 +0800

    Initial commit

---
+++ demo_ShopingCar/src/main/java/com/itqf/shopingcar/ShopingCarApplication.java
@@ -0,0 +1,20 @@
+package com.itqf.shopingcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * projectName: demo_end
+ *
+ * @author: xinxin
+ * time:  2020/11/411:15
+ * description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ShopingCarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopingCarApplication.class,args) ;
+    }
+}
---
+++ demo_ShopingCar/src/main/java/com/itqf/shopingcar/service/inte/CarService.java
@@ -0,0 +1,16 @@
+package com.itqf.shopingcar.service.inte;
+
+/**
+ * projectName: demo_end
+ *
+ * @author: xinxin
+ * time:  2020/11/417:00
+ * description:
+ */
+public interface CarService {
+
+    //实现购物车的添加
+
+
+
+}
